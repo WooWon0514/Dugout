9daf5910fda068db926c08b472969035efd656b8
WooWon0514 <chaeuwon70@gmail.com>
2025-05-29 21:45:55 +0900

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),  # 여기를 이렇게 수정!
-    path('kbo/', include('kbo.urls')),
-]
