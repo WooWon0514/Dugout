d9799611de3803bd9615c38d3148b136f22411c0
김민성 <rechardmin@gmail.com>
2025-06-02 22:15:07 +0900

    메인페이지 대충만들고 kbo파일 아예삭제 연관된것도 삭제해서 서버 정상작동

--- Dugout/Dugout/urls.py
+++ Dugout/Dugout/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # 여기를 이렇게 수정!
-    path('kbo/', include('kbo.urls')),
+    path('', views.index, name='index'),
 
 ]
