eddc7f98cc8328f19574a6d2499434ce69fb808b
김민성 <rechardmin@gmail.com>
2025-06-08 21:20:23 +0900

    requirements.txt추가해서 배포 했을시 다른 사용자가 명령어로설치가능하게 함

--- Dugout/Dugout/urls.py
+++ Dugout/Dugout/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from main import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # 여기를 이렇게 수정!
     path('teams/', include('teams.urls')),
     path('players/', include('players.urls')),
+    path('', main_views.main_page, name='main'),
 ]
