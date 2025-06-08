4797a856c52e325eeaa40c81c3270e1f1a15ec83
WooWon0514 <chaeuwon70@gmail.com>
2025-06-08 17:09:25 +0900

    Update urls.py

--- Dugout/Dugout/urls.py
+++ Dugout/Dugout/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # 여기를 이렇게 수정!
-    path('', include('main.urls')),  # 메인페이지 루트에 매핑
-
-
+    path('teams/', include('teams.urls')),
+    path('players/', include('players.urls')),
 ]
